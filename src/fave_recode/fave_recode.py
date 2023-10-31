d37672e811bae373d585521a19a8f0e5c5a2b76a
JoFrhwld <JoFrhwld@gmail.com>
2023-10-31 17:15:42 -0400

    finessing imports

--- src/fave_recode/fave_recode.py
+++ src/fave_recode/fave_recode.py
@@ -1,3 +1,3 @@
 from aligned_textgrid.aligned_textgrid import AlignedTextGrid
 from aligned_textgrid.sequences.word_and_phone import Word, Phone
-from rule_classes import RuleSet
+from fave_recode.rule_classes import RuleSet
