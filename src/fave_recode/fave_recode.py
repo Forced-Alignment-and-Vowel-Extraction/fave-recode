af0d760479ab057e96ac55598ae9aadb73c580c9
JoFrhwld <JoFrhwld@gmail.com>
2023-11-04 17:07:32 -0400

    working towards processing

--- src/fave_recode/fave_recode.py
+++ src/fave_recode/fave_recode.py
@@ -5,6 +5,7 @@ from fave_recode.schemes import all_schemes
 import click
 import cloup
 from pathlib import Path
+import io
 
 @cloup.command()
 @cloup.option_group("Inputs", 
@@ -54,16 +55,59 @@ def cli(
     scheme = None
 ):
     rules = get_rules(scheme)
-    print(rules)
+    if input_file:
+        process_file(input_file, output_file, rules)
 
 
-def get_rules(scheme):
+def get_rules(
+        scheme: str
+    ) -> RuleSet:
     if scheme in all_schemes:
         return RuleSet(rule_path = all_schemes[scheme])
     scheme_path = Path(scheme)
     if scheme_path.is_file():
         return RuleSet(rule_path = str(scheme_path))
     raise Exception(f"Cannot find rule schema file: {scheme}")
+
+def process_file(
+        input_file: io.TextIOWrapper, 
+        output_file: str, 
+        scheme: RuleSet,
+        recode_stem = "_recoded"
+    ):
+    input_path = Path(input_file.name)
+    atg = validate_input_file(input_path)
+    if output_file:
+        output_path = Path(output_file)
+    else:
+        output_path = make_output_path(input_path, recode_stem)
+    
+    validate_output_file(output_path)
+
+def validate_input_file(
+        input_path: Path
+    ) -> AlignedTextGrid:
+    try:
+        atg = AlignedTextGrid(textgrid_path=str(input_path))
+    except:
+        if input_path.suffix != ".TextGrid":
+            raise Exception(f"Problem encountered. "\
+                            f"Perhaps input {str(input_path)} is not a TextGrid.")
+        raise Exception(f"Problem ecountered. "\
+                        f"Couldn't process input {str(input_path)}.")
+    
+    return atg
+
+def make_output_path(
+        input_path: Path,
+        recode_stem: str
+) -> Path:
+    
+    pass
+    
+
+def validate_output_file(output_path):
+    pass
 
 if __name__ == "__main__":
     cli()
