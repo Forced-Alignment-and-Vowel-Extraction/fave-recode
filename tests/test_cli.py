f86a349610015b06201c47e469cde584002981c0
JoFrhwld <JoFrhwld@gmail.com>
2023-11-05 14:42:30 -0500

    make output test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,8 @@
 from fave_recode.rule_classes import RuleSet
 from aligned_textgrid import AlignedTextGrid
 from fave_recode.fave_recode import get_rules, \
-                                    validate_input_file
+                                    validate_input_file, \
+                                    make_output_path
 from fave_recode.schemes import all_schemes
 from pathlib import Path
 import pytest
@@ -33,3 +34,32 @@ class TestCLIComponents:
             not_file = Path("fake.TextGrid")
             validate_input_file(not_file)
         assert exec_info2.match("Couldn't process input")
+
+    def test_make_output(self):
+        input_path = Path("dir/input.TextGrid")
+        recode_stem = "_recoded"
+        output_path = Path("output")
+
+        path1 = make_output_path(
+            input_path=input_path, 
+            recode_stem=recode_stem
+        )
+
+        assert path1.name == "input_recoded.TextGrid"
+        assert path1.parent == input_path.parent
+
+        path2 = make_output_path(
+            input_path=input_path,
+            recode_stem=recode_stem,
+            output_path=output_path
+        )
+
+        assert path2.name == "input_recoded.TextGrid"
+        assert path2.parent == output_path
+
+        with pytest.raises(Exception):
+            make_output_path(
+                input_path=input_path,
+                recode_stem=recode_stem,
+                output_path="fake.TextGrid"
+            )
