0c4a39c9784d3194b4094decb3e9d32425962317
JoFrhwld <JoFrhwld@gmail.com>
2023-11-06 12:47:25 -0500

    testing for inputs with monkeypatch

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,17 @@
 from fave_recode.rule_classes import RuleSet
 from aligned_textgrid import AlignedTextGrid
 from fave_recode.fave_recode import get_rules, \
+                                    ask_for_dir_creation,\
+                                    ask_for_file_overwrite, \
                                     validate_input_file, \
                                     make_output_path
 from fave_recode.schemes import all_schemes
 from pathlib import Path
 import pytest
-import unittest.mock
+
 class TestCLIComponents:
 
+    ## core fave_recode operations
     def test_get_rules(self):
         rules1 = get_rules("cmu2labov")
         assert isinstance(rules1, RuleSet)
@@ -19,21 +22,31 @@ class TestCLIComponents:
         schwa_path = Path().joinpath("tests", "test_data", "just_schwa.yml")
         rules2 = get_rules(str(schwa_path))
         assert isinstance(rules2, RuleSet)
-    
-    def test_validate_input(self):
-        tg_path = Path("tests/test_data/josef-fruehwald_speaker.TextGrid")
-        atg1 = validate_input_file(tg_path)
-        assert isinstance(atg1, AlignedTextGrid)
-    
-        with pytest.raises(Exception) as exec_info1:
-            not_tg = Path("tests/test_data/just_schwa.yml")
-            validate_input_file(not_tg)
-        assert exec_info1.match("is not a TextGrid")
 
-        with pytest.raises(Exception) as exec_info2:
-            not_file = Path("fake.TextGrid")
-            validate_input_file(not_file)
-        assert exec_info2.match("Couldn't process input")
+    ## support operations
+    def test_ask_for_dir_creation(self, monkeypatch: pytest.MonkeyPatch):
+        monkeypatch.setattr("builtins.input", lambda _: "y")
+        test1 = ask_for_dir_creation(Path())
+        assert test1
+
+        monkeypatch.setattr("builtins.input", lambda _: "n")
+        assert not ask_for_dir_creation(Path())
+
+        input_sequence = iter(["ok", "YES"])
+        monkeypatch.setattr("builtins.input", lambda _: next(input_sequence))
+        assert ask_for_dir_creation(Path())
+
+    def test_ask_for_file_overwrite(self, monkeypatch: pytest.MonkeyPatch):
+        monkeypatch.setattr("builtins.input", lambda _: "y")
+        test1 = ask_for_file_overwrite(Path())
+        assert test1
+
+        monkeypatch.setattr("builtins.input", lambda _: "n")
+        assert not ask_for_file_overwrite(Path())
+
+        input_sequence = iter(["ok", "YES"])
+        monkeypatch.setattr("builtins.input", lambda _: next(input_sequence))
+        assert ask_for_file_overwrite(Path())
 
     def test_make_output(self):
         input_path = Path("dir/input.TextGrid")
@@ -63,3 +76,18 @@ class TestCLIComponents:
                 recode_stem=recode_stem,
                 output_path="fake.TextGrid"
             )
+    
+    def test_validate_input(self):
+        tg_path = Path("tests/test_data/josef-fruehwald_speaker.TextGrid")
+        atg1 = validate_input_file(tg_path)
+        assert isinstance(atg1, AlignedTextGrid)
+    
+        with pytest.raises(Exception) as exec_info1:
+            not_tg = Path("tests/test_data/just_schwa.yml")
+            validate_input_file(not_tg)
+        assert exec_info1.match("is not a TextGrid")
+
+        with pytest.raises(Exception) as exec_info2:
+            not_file = Path("fake.TextGrid")
+            validate_input_file(not_file)
+        assert exec_info2.match("Couldn't process input")
