71523dbcfc7ce8aadc601837b8e32176bc275651
JoFrhwld <JoFrhwld@gmail.com>
2024-04-03 12:43:57 -0400

    tests

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,5 +1,5 @@
 import pytest
-from fave_recode.schemes import cmu2labov_path, cmu2phila_path
+from fave_recode.schemes import cmu2labov_path, cmu2phila_path, cmu_parser_path
 from pathlib import Path
 
 class TestResoures:
@@ -8,3 +8,6 @@ class TestResoures:
     
     def test_cmu2phila(self):
         assert Path(cmu2phila_path).is_file()
+
+    def test_cmu_parser(self):
+        assert Path(cmu_parser_path).is_file()
