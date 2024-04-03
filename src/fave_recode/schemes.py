52d0ff730878f25191589613d1bafaf33d3a1967
JoFrhwld <JoFrhwld@gmail.com>
2024-04-03 12:43:49 -0400

    update parser resource

--- src/fave_recode/schemes.py
+++ src/fave_recode/schemes.py
@@ -2,7 +2,7 @@ from importlib.resources import files
 
 cmu2labov_path = str(files("fave_recode").joinpath("resources", "cmu2labov.yml"))
 cmu2phila_path = str(files("fave_recode").joinpath("resources", "cmu2phila.yml"))
-cmu_parser_path = str(files("fave_recode")).joinpath("resources", "cmu_parser.yml")
+cmu_parser_path = str(files("fave_recode").joinpath("resources", "cmu_parser.yml"))
 
 all_schemes = {
     "cmu2labov": cmu2labov_path,
